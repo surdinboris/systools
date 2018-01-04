8277c02080acc0a9ede232869747d964cea159b1
borrik <borrik@live.ru>
2018-01-04 16:29:35 +0200

    errorhandling added

--- dirtree/dirtree.py
+++ dirtree/dirtree.py
@@ -1,28 +1,43 @@
 import os
 import sys
 sys.setrecursionlimit(10000000)
+basedir = 'c:/'
+searchpf='1115-connection.xlsx'
+result=[]
+errors={}
 
-basedir = 'c:/gitrep/'
+def errorhandl(func):
+    def wrapper(*args,**kwargs):
+        try:
+            func(*args,**kwargs)
+        except (FileNotFoundError, PermissionError):
+            type, value, traceback = sys.exc_info()
+            errors[args] = ('Error opening %s: %s' % (value.filename, value.strerror))
+            pass
+    return wrapper
 
-
-def dirrec(sordir):
+@errorhandl
+def mainiter(sordir):
     tre = {}
-    try:
-        for subitem in os.listdir(sordir):
-
-                if os.path.isdir(os.path.join(sordir,subitem)):
-                    tre[subitem]={sordir:'dir'}
-
-                if os.path.isfile(os.path.join(sordir,subitem)):
-                    tre[subitem] = {sordir: 'fil'}
-    except FileNotFoundError:
-        pass
-
+    for subitem in os.listdir(sordir):
+        fullpath = os.path.join(sordir, subitem)
+        #print("searching in %s" %subitem)
+        if os.path.isdir(fullpath):
+            tre[subitem] = {sordir: 'dir'}
+            if searchpf == subitem:
+                print('found')
+                result.append(fullpath)
+        if os.path.isfile(fullpath):
+            tre[subitem] = {sordir: 'fil'}
+            if searchpf == subitem:
+                print('found')
+                result.append(fullpath)
     for ki, it in tre.items():
         for k, i in it.items():
             if i == 'dir':
-                dirrec(ki)
-    print(tre)
-dirrec(basedir)
+                nloop=os.path.join(sordir, ki)
+                mainiter(nloop)
 
-
+mainiter(basedir)
+print(result)
+print(errors)
